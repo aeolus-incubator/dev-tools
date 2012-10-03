be6d0eb7d95b6af6bfeeab25b8144eac24411f7d
Crag Wolfe <cwolfe@redhat.com>
2012-10-02 18:33:40 -0700

    Beat it into Puppet's declarative little head that I want the stuff at the top of the file to be executed first

--- test.pp
+++ test.pp
@@ -1,20 +1,25 @@
-if $rbenv_version == undef {
-  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
-         logoutput => "on_failure" }
-} else {
-  Exec { path => [ "~/.rbenv/bin", "~/.rbenv/shims", "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
-         logoutput => "on_failure" }
+class aeolus_dev_tools_path {
+  if $rbenv_version == undef {
+    Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
+           logoutput => "on_failure" }
+  } else {
+    Exec { path => [ "~/.rbenv/bin", "~/.rbenv/shims", "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
+           logoutput => "on_failure" }
+  }
+  # if not defined by facter
+  if $aeolus_workdir == undef {
+    $aeolus_workdir = '/tmp'
+  }
 }
 
-# if not defined by facter
-if $aeolus_workdir == undef {
-  $aeolus_workdir = '/tmp'
+class aeolus_dev_tools_install inherits aeolus_dev_tools_path {
+  if $id == 'root' {
+    # only install system dependencies
+    class { conductor: dev => true }
+  } else {
+    class { aeolus-image-rubygem: dev => true } -> class { conductor: dev => true }
+    class { aeolus-cli: dev => true }
+  }
 }
 
-if $id == 'root' {
-  # only install system dependencies
-  class { conductor: dev => true }
-} else {
-  class { aeolus-image-rubygem: dev => true } -> class { conductor: dev => true }
-  class { aeolus-cli: dev => true }
-}
+class {'aeolus_dev_tools_install':}

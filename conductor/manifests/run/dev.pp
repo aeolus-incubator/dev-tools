e8b718944da7ad75dad247512db3c1e0cbec2f58
Crag Wolfe <cwolfe@redhat.com>
2012-10-04 18:05:50 -0700

    rails server command needs to be in quotes due to the -p flag

--- conductor/manifests/run/dev.pp
+++ conductor/manifests/run/dev.pp
@@ -8,7 +8,7 @@ class conductor::run::dev {
   exec { "conductor rails server":
     cwd => "${aeolus_workdir}/conductor/src",
     #    command => 'bundle exec "rails server --daemon"',
-    command => "bundle exec rails server -p $conductor_port&",
+    command => "bundle exec \"rails server -p $conductor_port\"&",
     unless => "/usr/bin/curl http://0.0.0.0:$conductor_port"
   }
 }

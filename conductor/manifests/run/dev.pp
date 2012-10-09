aab764a64575ee1514ebe277342332f3527d7aaf
Crag Wolfe <cwolfe@redhat.com>
2012-10-09 12:12:00 -0700

    Add an exec that waits for the rails server to start.
    Thus, anything scripted afterwards can assume rails is ready to take requests
    (to say, add providers via the api).

--- conductor/manifests/run/dev.pp
+++ conductor/manifests/run/dev.pp
@@ -11,4 +11,12 @@ class conductor::run::dev {
     command => "bundle exec \"rails server -p $conductor_port\"&",
     unless => "/usr/bin/curl http://0.0.0.0:$conductor_port"
   }
+  exec { "wait for rails to start":
+    # since we backgrounded starting up rails, let's make sure it started up
+    cwd => "${aeolus_workdir}/conductor/src",
+    command => "/bin/sh -c 'while ! lsof -i :$conductor_port; do sleep 1; done'",
+    # give rails 45 seconds to start up or give up
+    timeout => 45,
+    require => Exec["conductor rails server"]
+  }
 }
